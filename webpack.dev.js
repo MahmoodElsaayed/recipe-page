74f375912b46bbd4b745882807341cafbba174c0
MahmoodElsaayed <Mahmood.M.Elsaayed@gmail.com>
2024-07-13 11:19:05 -0700

    Set up dev environment and boilerplates

---
+++ webpack.dev.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        client: {
+            overlay: {
+                warnings: false,
+                errors: true,
+            },
+        },
+        port: 8080,
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+        watchFiles: {
+            paths: ['src/**/*.*'],
+            options: {
+                usePolling: true,
+                ignored: /node_modules/,
+            },
+        },
+    }
+});
